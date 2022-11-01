1b8f884ac3cf58b247ff9624df99488033d4f401
Sanctorum Thomas <sanctorum.thomas@gmail.com>
2022-11-01 18:54:13 +0100

    change spawnpoints

---
+++ Contents/mods/ez_modpack/media/maps/EZ_LastRoad/spawnpoints.lua
@@ -0,0 +1,33 @@
+function SpawnPoints()
+    return {
+        unemployed = {
+            -- small gas station full north - west
+            { worldX = 12, worldY = 19, posX = 58, posY = 268, posZ = 0 },
+            { worldX = 12, worldY = 19, posX = 59, posY = 251, posZ = 0 },
+
+            -- full west, gas station with dinner and some house
+            { worldX = 12, worldY = 28, posX = 97, posY = 102, posZ = 0 },
+            { worldX = 12, worldY = 28, posX = 96, posY = 77, posZ = 0 },
+
+            -- south of RoseWood, bus station
+            { worldX = 27, worldY = 40, posX = 190, posY = 215, posZ = 0 },
+            { worldX = 27, worldY = 40, posX = 157, posY = 232, posZ = 0 },
+
+            -- gas station muldraugh north
+            { worldX = 38, worldY = 29, posX = 67, posY = 90, posZ = 0 },
+            { worldX = 38, worldY = 29, posX = 103, posY = 127, posZ = 0 },
+
+            -- gas station crossroad muldraugh - westpoint
+            { worldX = 38, worldY = 27, posX = 200, posY = 208, posZ = 0 },
+            { worldX = 38, worldY = 27, posX = 200, posY = 200, posZ = 0 },
+
+            -- gas station Fallas Lake
+            { worldX = 24, worldY = 27, posX = 112, posY = 84, posZ = 0 },
+            { worldX = 24, worldY = 27, posX = 116, posY = 96, posZ = 0 },
+
+            -- gas station Fallas Lake
+            { worldX = 42, worldY = 16, posX = 133, posY = 250, posZ = 0 },
+            { worldX = 42, worldY = 16, posX = 147, posY = 243, posZ = 0 },
+        }
+    }
+end
