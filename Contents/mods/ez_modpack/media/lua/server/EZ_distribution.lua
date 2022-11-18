6ba480927342051475270cc1a4a6e71023bfa7e4
Sanctorum Thomas <sanctorum.thomas@gmail.com>
2022-11-18 01:12:28 +0100

    update mod ID, add new crafting branch/twings oriented

--- Contents/mods/ez_modpack/media/lua/server/EZ_distribution.lua
+++ Contents/mods/ez_modpack/media/lua/server/EZ_distribution.lua
@@ -10,57 +10,57 @@ local kefir_jar_rand = 2
 local kefir_bottle_rand = 5
 
 -- kefir Grain
-table.insert(ProceduralDistributions.list["KitchenRandom"]["junk"].items, "EZ_MP.EZ_KefirSeed");
+table.insert(ProceduralDistributions.list["KitchenRandom"]["junk"].items, "EZ_LR.EZ_KefirSeed");
 table.insert(ProceduralDistributions.list["KitchenRandom"]["junk"].items, kefir_seed_rand/2);
 
-table.insert(ProceduralDistributions.list["KitchenRandom"].items, "EZ_MP.EZ_KefirSeed");
+table.insert(ProceduralDistributions.list["KitchenRandom"].items, "EZ_LR.EZ_KefirSeed");
 table.insert(ProceduralDistributions.list["KitchenRandom"].items, kefir_seed_rand);
 
-table.insert(ProceduralDistributions.list["BarCounterMisc"].items, "EZ_MP.EZ_KefirSeed");
+table.insert(ProceduralDistributions.list["BarCounterMisc"].items, "EZ_LR.EZ_KefirSeed");
 table.insert(ProceduralDistributions.list["BarCounterMisc"].items, kefir_seed_rand);
 
-table.insert(ProceduralDistributions.list["KitchenBreakfast"].items, "EZ_MP.EZ_KefirSeed");
+table.insert(ProceduralDistributions.list["KitchenBreakfast"].items, "EZ_LR.EZ_KefirSeed");
 table.insert(ProceduralDistributions.list["KitchenBreakfast"].items, kefir_seed_rand);
 
-table.insert(ProceduralDistributions.list["BreakRoomShelves"].items, "EZ_MP.EZ_KefirSeed");
+table.insert(ProceduralDistributions.list["BreakRoomShelves"].items, "EZ_LR.EZ_KefirSeed");
 table.insert(ProceduralDistributions.list["BreakRoomShelves"].items, kefir_seed_rand);
 
-table.insert(ProceduralDistributions.list["BreakRoomCounter"].items, "EZ_MP.EZ_KefirSeed");
+table.insert(ProceduralDistributions.list["BreakRoomCounter"].items, "EZ_LR.EZ_KefirSeed");
 table.insert(ProceduralDistributions.list["BreakRoomCounter"].items, kefir_seed_rand);
 
 -- kefir Jar
-table.insert(ProceduralDistributions.list["KitchenRandom"]["junk"].items, "EZ_MP.EZ_KefirJar");
+table.insert(ProceduralDistributions.list["KitchenRandom"]["junk"].items, "EZ_LR.EZ_KefirJar");
 table.insert(ProceduralDistributions.list["KitchenRandom"]["junk"].items, kefir_jar_rand/2);
 
-table.insert(ProceduralDistributions.list["KitchenRandom"].items, "EZ_MP.EZ_KefirJar");
+table.insert(ProceduralDistributions.list["KitchenRandom"].items, "EZ_LR.EZ_KefirJar");
 table.insert(ProceduralDistributions.list["KitchenRandom"].items, kefir_jar_rand);
 
-table.insert(ProceduralDistributions.list["BarCounterMisc"].items, "EZ_MP.EZ_KefirJar");
+table.insert(ProceduralDistributions.list["BarCounterMisc"].items, "EZ_LR.EZ_KefirJar");
 table.insert(ProceduralDistributions.list["BarCounterMisc"].items, kefir_jar_rand);
 
-table.insert(ProceduralDistributions.list["KitchenBreakfast"].items, "EZ_MP.EZ_KefirJar");
+table.insert(ProceduralDistributions.list["KitchenBreakfast"].items, "EZ_LR.EZ_KefirJar");
 table.insert(ProceduralDistributions.list["KitchenBreakfast"].items, kefir_jar_rand);
 
-table.insert(ProceduralDistributions.list["BreakRoomShelves"].items, "EZ_MP.EZ_KefirJar");
+table.insert(ProceduralDistributions.list["BreakRoomShelves"].items, "EZ_LR.EZ_KefirJar");
 table.insert(ProceduralDistributions.list["BreakRoomShelves"].items, kefir_jar_rand);
 
-table.insert(ProceduralDistributions.list["BreakRoomCounter"].items, "EZ_MP.EZ_KefirJar");
+table.insert(ProceduralDistributions.list["BreakRoomCounter"].items, "EZ_LR.EZ_KefirJar");
 table.insert(ProceduralDistributions.list["BreakRoomCounter"].items, kefir_jar_rand);
 
 -- Fridge Kefir Bottle
-table.insert(ProceduralDistributions.list["DeepFryKitchenFridge"].items, "EZ_MP.EZ_FilteredKefirBottle");
+table.insert(ProceduralDistributions.list["DeepFryKitchenFridge"].items, "EZ_LR.EZ_FilteredKefirBottle");
 table.insert(ProceduralDistributions.list["DeepFryKitchenFridge"].items, kefir_bottle_rand);
 
-table.insert(ProceduralDistributions.list["DinerKitchenFridge"].items, "EZ_MP.EZ_FilteredKefirBottle");
+table.insert(ProceduralDistributions.list["DinerKitchenFridge"].items, "EZ_LR.EZ_FilteredKefirBottle");
 table.insert(ProceduralDistributions.list["DinerKitchenFridge"].items, kefir_bottle_rand);
 
-table.insert(ProceduralDistributions.list["BarCounterMisc"].items, "EZ_MP.EZ_FilteredKefirBottle");
+table.insert(ProceduralDistributions.list["BarCounterMisc"].items, "EZ_LR.EZ_FilteredKefirBottle");
 table.insert(ProceduralDistributions.list["BarCounterMisc"].items, kefir_bottle_rand);
 
-table.insert(ProceduralDistributions.list["FridgeGeneric"].items, "EZ_MP.EZ_FilteredKefirBottle");
+table.insert(ProceduralDistributions.list["FridgeGeneric"].items, "EZ_LR.EZ_FilteredKefirBottle");
 table.insert(ProceduralDistributions.list["FridgeGeneric"].items, kefir_bottle_rand);
 
-table.insert(ProceduralDistributions.list["FridgeRich"].items, "EZ_MP.EZ_FilteredKefirBottle");
+table.insert(ProceduralDistributions.list["FridgeRich"].items, "EZ_LR.EZ_FilteredKefirBottle");
 table.insert(ProceduralDistributions.list["FridgeRich"].items, kefir_bottle_rand);
 
 ----------- END KEFIR -----------
--- Contents/mods/ez_modpack/media/lua/server/EZ_main.lua
+++ Contents/mods/ez_modpack/media/lua/server/EZ_main.lua
@@ -1,34 +1,34 @@
 -- Display a message to tell people to try to sleep
-function EZ_checkSleepTime()
-    if getGameTime():getHour() % 24 == 22 then
-        local player = getPlayer();
-        if player == nil then
-            return;
-        end
-        player:Say(getText("IGUI_PlayerText_ez_sleep"));
-    end
-end
+-- function EZ_checkSleepTime()
+--     if getGameTime():getHour() % 24 == 22 then
+--         local player = getPlayer();
+--         if player == nil then
+--             return;
+--         end
+--         player:Say(getText("IGUI_PlayerText_ez_sleep"));
+--     end
+-- end
 
 -- Play Church Bell on player death to notify other player
-function EZ_playerDeath(player)
-    local serverPlayer = getPlayer();
+-- function EZ_playerDeath(player)
+--     local serverPlayer = getPlayer();
 
-    if serverPlayer == nil or serverPlayer:getUsername() == player:getUsername() then
-        return;
-    end
+--     if serverPlayer == nil or serverPlayer:getUsername() == player:getUsername() then
+--         return;
+--     end
 
-    local churchBellSound = "church_bell";
-    local sound = getSoundManager():PlaySound(churchBellSound, false, 0);
-    getSoundManager():PlayAsMusic(churchBellSound, sound, false, 0);
-    sound:setVolume(0.3);
-end
+--     local churchBellSound = "church_bell";
+--     local sound = getSoundManager():PlaySound(churchBellSound, false, 0);
+--     getSoundManager():PlayAsMusic(churchBellSound, sound, false, 0);
+--     sound:setVolume(0.3);
+-- end
 
 -- regive to player jar & more kefir seed
 function EZ_recipe_Process_Kefir(items, result, player)
     EZ_recipe_Process_Jar(items, result, player);
 
     -- recipe coast + 1
-    player:getInventory():AddItems("EZ_MP.EZ_KefirSeed", 6);
+    player:getInventory():AddItems("EZ_LR.EZ_KefirSeed", 6);
 end
 
 -- use the new 'FullJar' tags in recipe
@@ -46,6 +46,22 @@ function EZ_recipe_Empty_Jar(items, result, player)
     player:getInventory():AddItem("Base.EmptyJar");
 end
 
+-- save the ropes used
+function EZ_recipe_CreateStack(items, result, player)
+    local item, itemType;
+    local ropeItems = {};
+    for i = 0, items:size() - 1 do
+        item = items:get(i);
+        if item then
+            itemType = item:getFullType();
+            if itemType ~= "Base.Twigs" and itemType ~= "Base.TreeBranch" then
+                table.insert(ropeItems, itemType);
+            end;
+        end;
+    end;
+    result:getModData().ropeItems = ropeItems;
+end
+
 -- --- EVENTS MANAGER ---
-Events.EveryHours.Add(EZ_checkSleepTime);
-Events.OnPlayerDeath.Add(EZ_playerDeath);
+-- Events.EveryHours.Add(EZ_checkSleepTime);
+-- Events.OnPlayerDeath.Add(EZ_playerDeath);
